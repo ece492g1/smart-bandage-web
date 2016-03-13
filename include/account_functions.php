a021d72f5f755811582f5db1f3c87514a82c75cb
Jared Cuglietta <jbcuglietta@gmail.com>
2016-03-13 13:45:18 -0600

    account functions added

--- include/account_functions.php
+++ include/account_functions.php
@@ -70,8 +70,10 @@
 
     if ($r0){
       $errors[] = "User has been deactivated";
+      return(true);
     } else {
       $errors[] ="User could not be deactivated";
+      return(false);
     }
   }
 
@@ -80,7 +82,7 @@
 
 		$q1 = "SELECT email, password FROM users WHERE email = '$email' AND password = '$oldpass'";
 		$r1 = @mysqli_query($dbc,$q1);
-		
+
 		if (mysqli_num_rows($r1) == 1 ){
 
 			$q2 = "UPDATE users SET password = '$newpass' WHERE email = '$email'";
@@ -99,4 +101,96 @@
 		}
 	}
 
+  function updateUserFirstName($dbc,$pid,$firstname) {
+    $errors = array();
+
+    $q0 = "UPDATE TABLE users SET first_name = $firstname WHERE provider_id = $pid";
+    $r0 = mysqli_query($dbc,$q0);
+
+    if ($r0) {
+      $errors[] = 'First name updated';
+      return(true,$errors);
+    }
+    else {
+      $errors[] ="Unable to update first name";
+      return(false,$errors);
+    }
+  }
+
+  function updateUserLastName($dbc,$pid,$lastname) {
+    $errors = array();
+
+    $q0 = "UPDATE TABLE users SET last_name = $lastname WHERE provider_id = $pid";
+    $r0 = mysqli_query($dbc,$q0);
+
+    if ($r0) {
+      $errors[] = 'Last name updated';
+      return(true,$errors);
+    }
+    else {
+      $errors[] ="Unable to update last name";
+      return(false,$errors);
+    }
+  }
+
+  function updateUserEmail($dbc,$pid,$email) {
+    $errors = array();
+
+    $q0 = "UPDATE TABLE users SET email = $email WHERE provider_id = $pid";
+    $r0 = mysqli_query($dbc,$q0);
+
+    if ($r0) {
+      $errors[] = 'Email updated';
+      return(true,$errors);
+    }
+    else {
+      $errors[] ="Unable to update email";
+      return(false,$errors);
+    }
+  }
+
+  function updateUserType($dbc,$pid,$type) {
+    $errors = array();
+
+    $q0 = "UPDATE TABLE users SET user_type = $type WHERE provider_id = $pid";
+    $r0 = mysqli_query($dbc,$q0);
+
+    if ($r0) {
+      $errors[] = 'User type updated';
+      return(true,$errors);
+    }
+    else {
+      $errors[] ="Unable to update user type";
+      return(false,$errors);
+    }
+  }
+
+  function addPatient($dbc,$first,$last) {
+		$errors = array();
+		$rollback = false;
+		$q0 = "SELECT * FROM users WHERE first_name = '$first' AND last_name = '$last'";
+		$r0 = mysqli_query($dbc,$q0);
+		if (mysqli_num_rows($r0) > 0){
+			$errors[] = 'patient exists';
+			return array(false, $errors);
+		}
+		mysqli_autocommit($dbc,false);
+
+		$q2 = "INSERT INTO patient (first_name,last_name) VALUES ('$first', '$last')";
+		$r2 = mysqli_query($dbc, $q2);
+
+		if ($r2){ // the insert was successful
+			mysqli_commit($dbc);
+			$errors[] = 'Successful Inserted Patient';
+			return array(true,$errors);
+		} else { // insert into users failed
+			$rollback = true;
+			$errors[] = 'Failed to Insert Patient';
+		}
+
+		if ($rollback){
+			mysqli_rollback($dbc);
+			return array(false,$errors);
+		}
+	}
 ?>
