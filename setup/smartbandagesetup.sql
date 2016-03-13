2a605d1dab05b6b85bdcc2ba629f4d8fd7257687
Jared Cuglietta <jbcuglietta@gmail.com>
2016-03-13 11:38:20 -0600

    added inactive users table, so users are never really deleted

--- setup/smartbandagesetup.sql
+++ setup/smartbandagesetup.sql
@@ -20,6 +20,18 @@ CREATE TABLE users (
   UNIQUE (email)
 );
 
+CREATE TABLE inactive_users (
+  provider_id INT(5) UNSIGNED,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  password VARCHAR(30) NOT NULL,
+  user_type CHAR(1) NOT NULL,
+  CHECK (user_type in ('a', 'n')),
+  PRIMARY KEY (provider_id),
+  UNIQUE (email)
+);
+
 CREATE TABLE login_history (
   provider_id INT(5) UNSIGNED NOT NULL,
   login_date DATETIME,
