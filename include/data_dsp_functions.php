2e907781f4559c60592024fc64a926450d1605a1
Jared Cuglietta <jbcuglietta@gmail.com>
2016-03-15 16:23:50 -0600

    added functions to format data for charting, incomplete

--- include/data_dsp_functions.php
+++ include/data_dsp_functions.php
@@ -1,14 +1,48 @@
 <?php
   function getTempData($dbc,$patientId,$bandageId,$year,$month,$day){
+    $errors = array();
+    $q0 = "SELECT creation_time, value FROM temp_record WHERE patient_id = $patientId AND bandage_id = $bandageId AND YEAR(creation_time) ='$year' AND MONTHNAME(creation_time) = '$month' AND DAYOFMONTH(creation_time) = '$day' ORDER BY creation_time";
+    $r0 = mysqli_query($dbc,$q0);
+    if ($r0){
+      $labels = array();
+      $data = array();
+    while ($row = mysqli_fetch_array($r0, MYSQLI_ASSOC)) {
+      $labels[] = $row['creation_time'];
+      $data[] = $row['value'];
+    }
+    return array($labels,$data);
+  }
 
   }
 
   function getHumidityData($dbc,$patientId,$bandageId,$year,$month,$day){
-
+    $errors = array();
+    $q0 = "SELECT creation_time, value FROM humidity_record WHERE patient_id = $patientId AND bandage_id = $bandageId AND YEAR(creation_time) ='$year' AND MONTHNAME(creation_time) = '$month' AND DAYOFMONTH(creation_time) = '$day' ORDER BY creation_time";
+    $r0 = mysqli_query($dbc,$q0);
+    if ($r0){
+      $labels = array();
+      $data = array();
+    while ($row = mysqli_fetch_array($r0, MYSQLI_ASSOC)) {
+      $labels[] = $row['creation_time'];
+      $data[] = $row['value'];
+    }
+    return array($labels,$data);
+  }
   }
 
   function getMoistureData($dbc,$patientId,$bandageId,$year,$month,$day){
-
+    $errors = array();
+    $q0 = "SELECT creation_time, value FROM moisture_record WHERE patient_id = $patientId AND bandage_id = $bandageId AND YEAR(creation_time) ='$year' AND MONTHNAME(creation_time) = '$month' AND DAYOFMONTH(creation_time) = '$day' ORDER BY creation_time";
+    $r0 = mysqli_query($dbc,$q0);
+    if ($r0){
+      $labels = array();
+      $data = array();
+    while ($row = mysqli_fetch_array($r0, MYSQLI_ASSOC)) {
+      $labels[] = $row['creation_time'];
+      $data[] = $row['value'];
+    }
+    return array($labels,$data);
+  }
   }
 
  ?>
