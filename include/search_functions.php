6107deff0529df5b87f55a60e07d55983e1ed470
Jared Cuglietta <jbcuglietta@gmail.com>
2016-03-20 00:07:02 -0600

    added patientSearch, tabulate

--- include/search_functions.php
+++ include/search_functions.php
@@ -1,6 +1,6 @@
 <?php
   function patientSearch($dbc,$searchParam){
-    $errors array();
+    $errors = array();
     $q0 = "SELECT * from patient WHERE patient_id LIKE '%$seachParam%' OR first_name LIKE '%$searchParam%' OR last_name LIKE '%$searchParam%'";
     $r0 = mysqli_query($dbc,$q0);
     if ($r0){
@@ -9,5 +9,39 @@
       $errors[] = "No rows matching this search found";
       return array(false,$errors);
     }
+  }
+
+
+function tabulateResultSet($rs){
+  $info = mysqli_fetch_fields($rs);
+  $table = "<table class='table table-striped table-bordered'>";
+  $table .= "<tr>";
+  foreach($info as $inforow){
+    $table .= "<th>" . $inforow->name . "</th>";
+  }
+  $table .= "</tr>";
+  while ($row = mysqli_fetch_array($rs, MYSQLI_ASSOC)) {
+    $table .= "<tr>";
+    foreach ($row as $rowele){
+      $table.= "<td>". $rowele . "</td>";
+    }
+    $table .= "</tr>";
+  }
+  $table .= "</table>";
+  return $table;
+}
+
+function getSubscriptions($dbc,$pid){
+  $errors = array();
+      $q = "SELECT patient.last_name AS 'Last Name',patient.first_name AS 'First Name' FROM patient INNER JOIN subscriptions ON patient.patient_id = subscriptions.patient_id WHERE subscriptions.care_provider = $pid";
+      $r = @mysqli_query($dbc,$q);
+
+  if (mysqli_num_rows($r) >= 1) {
+          return array(true,$r);
+      }
+      else {
+          $errors[] = 'This person does not have any subscriptions';
+    return array(false, $errors);
+      }
   }
  ?>
