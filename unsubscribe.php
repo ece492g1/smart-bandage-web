9c7fe6c526163b2af752d8aaab0894afdd313032
Jared Cuglietta <jbcuglietta@gmail.com>
2016-03-23 20:35:27 -0600

    added sub/unsubscribe

---
+++ unsubscribe.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+  require('include/login_functions.php');
+  if ($_SESSION['user_type'] != 'n'){
+    redirect_user();
+  }
+  require('../sql_connect.php');
+  include('include/subscription_functions.php');
+  unsubscribeToPatient($dbc,$_POST['user'],$_POST['patient_id']);
+  mysqli_close($dbc);
+  redirect_user('nursehome.php');
+ ?>
