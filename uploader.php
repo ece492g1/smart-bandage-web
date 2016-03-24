09d625fae3add783edc6e4d9b0058e739fe7c2a2
Jared Cuglietta <jbcuglietta@gmail.com>
2016-03-23 22:47:57 -0600

    .

--- uploader.php
+++ uploader.php
@@ -3,10 +3,7 @@
 // File contains database connection information
 require('../sql_connect.php');
 require('include/uploader_functions.php');
-session_start();
-  if ($_SESSION['user_type'] != 'n'){
-    redirect_user();
-  }
+require('include/login_functions.php');
 $record_type = $_POST['record_type'];
 $bandage_id = $_POST['bandage_id'];
 $sensor_id = $_POST['sensor_id'];
@@ -14,7 +11,7 @@ $value = $_POST['value'];
 $creation_time = $_POST['creation_time'];
 
 if($record_type == 'temp'){
-  list ($ok,$errors) = addTemp($dbc,intval($bandage_id),intval($sensor_id),$creation_time,floatval($value));
+  list ($ok,$errors) = addTemp($dbc,$bandage_id,$sensor_id,$creation_time,$value);
   if (floatval($value) > floatval(25.5)){
     list($ok2,$errors2) = addAlert($dbc,$bandage_id,"t",$creation_time,$value);
   }
