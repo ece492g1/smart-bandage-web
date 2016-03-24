fb5756d841cf47620bd661a14b7060014d83f352
Jared Cuglietta <jbcuglietta@gmail.com>
2016-03-24 12:57:20 -0600

    sub/unsub cleaned

--- unsubscribe.php
+++ unsubscribe.php
@@ -1,12 +1,14 @@
 <?php
-session_start();
+  session_start();
   require('include/login_functions.php');
   if ($_SESSION['user_type'] != 'n'){
     redirect_user();
   }
-  require('../sql_connect.php');
-  include('include/subscription_functions.php');
-  unsubscribeToPatient($dbc,$_POST['user'],$_POST['patient_id']);
-  mysqli_close($dbc);
+  if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    require('../sql_connect.php');
+    include('include/subscription_functions.php');
+    unsubscribeToPatient($dbc,$_POST['user'],$_POST['patient_id']);
+    mysqli_close($dbc);
+  }
   redirect_user('nursehome.php');
  ?>
