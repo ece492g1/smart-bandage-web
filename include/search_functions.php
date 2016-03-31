5585bd2ffd045142426a175c771c2d5df818ff93
Jared Cuglietta <jbcuglietta@gmail.com>
2016-03-31 15:33:04 -0600

    added search function for users

--- include/search_functions.php
+++ include/search_functions.php
@@ -11,6 +11,31 @@
     }
   }
 
+  function nurseSearch($dbc,$searchParam){
+    $errors = array();
+    $q0 = "SELECT provider_id AS 'Provider_ID', last_name AS 'Last Name', first_name AS 'First Name', user_type AS 'USER TYPE' FROM users WHERE provider_id LIKE '%$searchParam%' OR first_name LIKE '%$searchParam%' OR last_name LIKE '%$searchParam%'";
+    $r0 = mysqli_query($dbc,$q0);
+    if ($r0){
+      return array(true,$r0);
+    }else {
+      $errors[] = "No rows matching this search found";
+      return array(false,$errors);
+    }
+  }
+
+  function getNurseInfo($dbc,$searchParam){
+    $errors = array();
+    $q0 = "SELECT * FROM users WHERE provider_id = '$searchParam'";
+    $r0 = mysqli_query($dbc,$q0);
+    if (mysqli_num_rows($r0) == 1) {
+        $row = mysqli_fetch_array($r0,MYSQLI_ASSOC);
+        return array(true,$row);
+      }
+    else {
+        $errors[] = 'There is not a person with this pid';
+        return array(false,$errors);
+      }
+  }
 
   function getPatientInfo($dbc,$searchParam){
     $errors = array();
@@ -46,6 +71,26 @@ function tabulateResultSet($rs){
   return $table;
 }
 
+function tabulateNurseResultSet($rs){
+  $info = mysqli_fetch_fields($rs);
+  $table = "<table class='table table-striped table-bordered'>";
+  $table .= "<tr>";
+  foreach($info as $inforow){
+    $table .= "<th>" . $inforow->name . "</th>";
+  }
+  $table .="<th>View</th>";
+  $table .= "</tr>";
+  while ($row = mysqli_fetch_array($rs, MYSQLI_ASSOC)) {
+    $table .= "<tr>";
+    foreach ($row as $rowele){
+      $table.= "<td>". $rowele . "</td>";
+    }
+    $table.= "<td><form action='nurseOverview.php' method='post'><input type='hidden' name='nurse_id' id='nurse_id' value=".$row['Provider ID']."><button type='submit' class='btn btn-primary'>Profile</button></form></td>";
+    $table .= "</tr>";
+  }
+  $table .= "</table>";
+  return $table;
+}
 function getSubscriptions($dbc,$pid){
   $errors = array();
       $q = "SELECT patient.last_name AS 'Last Name',patient.first_name AS 'First Name', patient.patient_id AS 'Patient ID' FROM patient INNER JOIN subscriptions ON patient.patient_id = subscriptions.patient_id WHERE subscriptions.care_provider = $pid";
